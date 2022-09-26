f8ab610492a052a5a6605533a41553efed0721c8
paramighty <avinnosatta@gmail.com>
2022-09-26 16:34:35 +0200

    Added Hamburger

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function on() {
 function off() {
     document.getElementById("overlay").style.display = "none";
 } */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
