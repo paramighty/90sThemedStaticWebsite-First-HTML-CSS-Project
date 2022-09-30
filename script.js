8e93e82ea979c5147be8ef8c1c31e44b87349003
paramighty <avinnosatta@gmail.com>
2022-09-30 10:29:12 +0200

    Added NewSite

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 } */
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+let toggleButton = document.getElementsByClassName('toggle-button')[0]
+let navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
