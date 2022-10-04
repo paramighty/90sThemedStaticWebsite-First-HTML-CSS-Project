c932778e6b6fea995bfd7e90b09e147e7c7976e4
paramighty <avinnosatta@gmail.com>
2022-10-04 07:44:34 +0200

    Mentor Commit

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 } */
 
+
+
 let toggleButton = document.getElementsByClassName('toggle-button')[0]
 let navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
